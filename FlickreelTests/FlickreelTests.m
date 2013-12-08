198f03ca27234583a42317d6eb81bccf6018c196
Austris Landmanis <aus3ys@gmail.com>
2013-12-08 16:52:38 +0200

    Initial Commit

---
+++ FlickreelTests/FlickreelTests.m
@@ -0,0 +1,34 @@
+//
+//  FlickreelTests.m
+//  FlickreelTests
+//
+//  Created by Austris Landmanis on 08/12/13.
+//  Copyright (c) 2013 Austris Landmanis. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FlickreelTests : XCTestCase
+
+@end
+
+@implementation FlickreelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
